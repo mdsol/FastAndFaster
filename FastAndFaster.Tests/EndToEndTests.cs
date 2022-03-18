eae40d3ca7f5bcb1ffb254e3dd743f65cfb683ef
Nguyen Thai Duong <thanguyen@mdsol.com>
2022-03-18 18:26:41 +0900

    Modify license file. Modify 1 unit test.

--- FastAndFaster.Tests/EndToEndTests.cs
+++ FastAndFaster.Tests/EndToEndTests.cs
@@ -11,17 +11,19 @@ namespace FastAndFaster.Tests
         {
             // Arrange, Act
             var typeName = typeof(InvocationTarget).AssemblyQualifiedName;
-            var methodName = nameof(InvocationTarget.ParameterlessAction);
+            var methodName = nameof(InvocationTarget.Action);
+            var argumentTypes = new[] { typeof(string), typeof(int) };
+            var arguments = new object[] { "DuongNT", 1989 };
             var initializer = Initializer.Create(typeName);
-            var invocator = Invocator.CreateAction(typeName, methodName);
+            var invocator = Invocator.CreateAction(typeName, methodName, argumentTypes);
 
             // Act
             var target = initializer(null) as InvocationTarget;
-            invocator(target, null);
+            invocator(target, arguments);
 
             // Assert
             target.Should().NotBeNull();
-            target.ParameterlessActionCalled.Should().BeTrue();
+            target.ActionData.Should().Be(("DuongNT", 1989));
         }
     }
 }
